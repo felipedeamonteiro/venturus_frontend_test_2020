e26564c8243d3f847fde6a4e0aeb09284a15bccc
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-11-30 19:35:27 -0300

    Add instructions to login page - wip

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,6 +1,25 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  height: 120vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+`;
+
+export const AnimationContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -20,4 +39,16 @@ export const Container = styled.div`
   h3 {
     margin-bottom: 30px;
   }
+`;
+
+export const Instructions = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  line-height: 35px;
+
+  place-content: center;
+
+  width: 100%;
 `;
